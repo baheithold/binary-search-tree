7469e264eb1702a81503bad73f9a613159e5d498
Brett Heithold <baheithold@crimson.ua.edu>
2018-02-05 06:40:48 -0600

    Testing BSTNODE methods

--- test-bst.c
+++ test-bst.c
@@ -9,7 +9,12 @@
 
 int main(void) {
 
-    BSTNODE *n = newBSTNODE(newINTEGER(17));
+    BSTNODE *n = newBSTNODE(newINTEGER(2));
+    setBSTNODEleft(n, newBSTNODE(newINTEGER(1)));
+    setBSTNODEright(n, newBSTNODE(newINTEGER(3)));
+    fprintf(stdout, "Left: %d\n", getINTEGER(getBSTNODEvalue(getBSTNODEleft(n))));
+    fprintf(stdout, "Center: %d\n", getINTEGER(getBSTNODEvalue(n)));
+    fprintf(stdout, "Right: %d\n", getINTEGER(getBSTNODEvalue(getBSTNODEright(n))));
 
     freeBSTNODE(n, freeINTEGER);
     return 0;
